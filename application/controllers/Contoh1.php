286dc9431f4e6dc2c5fa9eb786ca7fb2c7b7ee81
akbardtya <akbaraditya2004@gmail.com>
2024-03-27 19:32:28 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+    class Contoh1 extends CI_Controller {
+        public function index() {
+            echo "<h1>Perkenalan</h1>";
+            echo "Nama saya Akbar Aditya
+                  Saya tinggal di daerah Bekasi
+                  olah raga yang saya sukai adalah
+                  Sepakbola";
+        }
+    }
+?>
